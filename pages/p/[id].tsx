b2b93a2dfb166c04b7ac8ecec6d382bec669d3dc
Kendry Grullon <kengrullon@gmail.com>
2020-02-05 09:49:21 -0400

    Getting to work the post pages.

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,16 +1,35 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
 
 import Layout from "../../components/MyLayout";
 
-const Post: NextPage = () => {
-  const router = useRouter();
+type Show = {
+  name: string;
+  summary: string;
+  image?: {
+    medium?: string;
+    large?: string;
+  };
+};
+
+const Post: NextPage<{ show: Show }> = ({ show }) => {
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog content.</p>
+      <h1>{show.name}</h1>
+      <p>{show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+      {show.image ? <img src={show.image.medium} /> : null}
     </Layout>
   );
+};
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
 export default Post;
