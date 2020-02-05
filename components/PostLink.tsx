be559d544a522aedae04b32e3d7226ece2859d39
Kendry Grullon <kengrullon@gmail.com>
2020-02-05 09:29:54 -0400

    Adding unfetch and getting initialprops from a API.

--- components/PostLink.tsx
+++ components/PostLink.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 type PostLinkProps = {
   id: string;
+  name: string;
 };
 
 const PostLink = (props: PostLinkProps) => {
   return (
     <li>
       <Link href="/p/" as={`/p/${props.id}`}>
-        <a>{props.id}</a>
+        <a>{props.name}</a>
       </Link>
     </li>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,36 @@
 import { NextPage } from "next";
+import fetch from "isomorphic-unfetch";
 
 import Layout from "../components/MyLayout";
 import PostLink from "../components/PostLink";
 
-const Home: NextPage = () => (
+type Show = {
+  id: string;
+  name: string;
+};
+
+type tvMaze = {
+  show: Show;
+};
+
+const Home: NextPage<{ shows: Show[] }> = ({ shows }) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" />
-      <PostLink id="learn-nextjs" />
-      <PostLink id="deploy-zeit" />
+      {shows.map(show => {
+        return <PostLink key={show.id} id={show.id} name={show.name} />;
+      })}
     </ul>
   </Layout>
 );
+
+Home.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data: tvMaze[] = await res.json();
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
 export default Home;
