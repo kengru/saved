408475bce9b8477cb32a416e68f769027a780809
kengru <kengrullon@gmail.com>
2020-02-04 22:24:45 -0400

    Testing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+  <h1>Hello world!!! - user agent: {userAgent}</h1>
 );
 
 Home.getInitialProps = async ({ req }) => {
